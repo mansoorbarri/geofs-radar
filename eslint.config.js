a5a866306299a5fd3ae7861ad8261bad6b5a9049
Mansoor Barri <mansoor.eb.ak@gmail.com>
2025-11-29 21:00:20 +0000

    update filename in eslint and next.config for userscript

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   {
-    ignores: [".next", "geofs-radar.user.js"],
+    ignores: [".next", "userscript.js"],
   },
   ...compat.extends("next/core-web-vitals"),
   {
