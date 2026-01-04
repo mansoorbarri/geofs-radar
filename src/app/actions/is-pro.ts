e49aad63f0f130f86d9bf7b57e446f65a5beb399
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-01-04 04:22:07 +0000

    remove debugging logs

--- src/app/actions/is-pro.ts
+++ src/app/actions/is-pro.ts
@@ -5,14 +5,14 @@ import { db } from "~/server/db";
 
 export async function isPro() {
   const { userId } = await auth();
-  if (!userId) return false;
+  if (!userId) return null;
 
   const user = await db.user.findUnique({
     where: { clerkId: userId },
     select: { role: true },
   });
 
-  console.log("user", user)
+  // console.log("user", user)
 
   return user?.role === "PRO";
 }
