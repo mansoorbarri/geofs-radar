7e8a01f01d2265cc1020d1eae16ea7debd83d381
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-01-17 15:32:37 +0000

    update version #

--- geofs-radar.user.js
+++ geofs-radar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RadarThing
 // @namespace    http://tampermonkey.net/
-// @version      1.2.8
+// @version      1.2.9
 // @description  Always loads the latest GeoFS ATC Radar script from GitHub
 // @Author       xyzmani
 // @match        http://*/geofs.php*
